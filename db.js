49fef4d7f6be0799691ea55946f0328fcfa63f19
rohanshrestha09 <shresthacr11@gmail.com>
2022-09-03 20:49:19 +0545

    Refactor

--- db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = () => {
-  mongoose.connect(process.env.MONGODB_URI);
-};
-
-module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const connectDB = require('./db');
+const User = require('./model/Users');
 
 const port = process.env.PORT || 5000;
 
@@ -12,8 +13,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, 'client/build')));
 
-connectDB();
+mongoose.connect(process.env.MONGODB_URI);
 
-app.use('/contact', require('./routes/contact'));
+app.post(
+  '/api/contact',
+  asyncHandler(async (req, res) => {
+    const { name, email, subject, message } = req.body;
+
+    try {
+      await User.create({ name, email, subject, message });
+      res.sendStatus(200);
+    } catch (err) {
+      res.sendStatus(400);
+    }
+  })
+);
 
 app.listen(port);
